c46e96cbaa57c4ca7a8db0e2d8c2ade5c3f46916
edu-juan <hphk.juan@gmail.com>
2019-12-12 10:47:18 +0900

    191212 | dict_02

--- 01_python/00_python_basic/dict_01.py
+++ 01_python/00_python_basic/dict_01.py
@@ -50,6 +50,12 @@ score = {
 
 # 아래에 코드를 작성해 주세요.
 print('==== Q1 ====')
+sum_score = 0
+for s in score.values():
+    sum_score += s
+result = sum_score / len(score)
+print(result)
+
 
 # 2. 반 평균을 구하시오. -> 전체 평균
 scores = {
@@ -67,6 +73,15 @@ scores = {
 
 # 아래에 코드를 작성해 주세요.
 print('==== Q2 ====')
+sum_score = 0
+count = 0
+
+for person_score in scores.values():
+    for subject_score in person_score.values():
+        sum_score += subject_score
+        count += 1
+result = sum_score / count
+print(result)
 
 # 3. 도시별 최근 3일의 온도입니다.
 city = {
@@ -77,9 +92,41 @@ city = {
 }
 
 # 3-1. 도시별 최근 3일의 온도 평균은?
+print('==== Q3-1 ====')
+for key, value in city.items():
+    temps = 0
+    for t in value:
+        temps += t
+    temp = temps / len(value)
+    print(f'{key}: {temp}')
 
 # 3-2. 도시 중에 최근 3일 중에 가장 추웠던 곳, 가장 더웠던 곳은?
+print('==== Q3-2 ====')
+
+count = 0
+for key, value in city.items():
+    # 최초 시행시에만 적용되는 조건문
+    if count == 0:
+        hot_temp = max(value)
+        cold_temp = min(value)
+        hot_city = key
+        cold_city = key
+        count += 1
+    else:
+        if max(value) > hot_temp:
+            hot_temp = max(value)
+            hot_city = key
+        if min(value) < cold_temp:
+            cold_temp = min(value)
+            cold_city = key
+print(hot_city, hot_temp, cold_city, cold_temp)
 
 # 3-3. 위에서 서울은 영상 2도였던 적이 있나요?
+print('==== Q3-3 ====')
+
+if 2 in city['서울']:
+    print('예')
+else:
+    print('아니오')
 
 # 아래에 코드를 작성해 주세요.
---
+++ 01_python/00_python_basic/dict_02.py
@@ -0,0 +1,81 @@
+import random
+
+t4ir_6th = {
+    'division': ['머신러닝', '빅데이터', 'IoT', 'VR'],
+    'language': {
+        'python': {
+            'python standard library': ['os', 'random', 'webbrowser'],
+            'frameworks': {
+                'flask': 'micro',
+                'django': 'full-functioning'
+            },
+            'data_science': ['numpy', 'pandas', 'scipy', 'sklearn'],
+            'scraping': ['requests', 'bs4'],
+        },
+        'web' : ['HTML', 'CSS']
+    },
+    'classes': {
+        '1305': {
+            'lecturer': 'justin',
+            'manager': '문주영',
+            'class president': '문한나',
+            'groups': {
+                'A': ['문한나', '박나영', '김연주'],
+                'B': ['백대현', '김완규', '윤동현'],
+                'C': ['장민재', '강현수', '강동욱', '장하림'],
+                'D': ['김이슬', '최희은', '전국현', '한상윤'],
+                'E': ['최여진', '조현민', '김나윤', '유한솔']
+            }
+        },
+        '503': {
+            'lecturer': 'change',
+            'manager': '신윤미'
+        }
+    }
+}
+
+
+"""
+난이도* 1. 반 분류(division)는 몇 개 있나요?
+출력예시)
+4
+"""
+
+"""
+난이도** 2. python standard library에 'requests'가 있나요?
+출력예시)
+False
+"""
+
+"""
+난이도** 3. 1305호의 반장의 이름을 출력하세요.
+출력예시)
+문한나
+"""
+
+"""
+난이도*** 4. t4ir에서 배우는 언어들을 출력하세요.
+출력 예시)
+python
+web
+"""
+
+"""
+난이도*** 5 t4ir 503호의 강사와 매니저의 이름을 출력하세요.
+출력 예시)
+change
+신윤미
+"""
+
+"""
+난이도***** 6. framework 들의 이름과 설명을 다음과 같이 출력하세요.
+출력 예시)
+flask는 micro이다.
+django는 full-functioning이다.
+"""
+
+"""
+난이도***** 7. 오늘 당번을 뽑기 위해 groups의 E 그룹에서 한명을 랜덤으로 뽑아주세요.
+출력예시)
+오늘의 당번은 최여진
+"""
